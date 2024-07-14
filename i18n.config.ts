cc88ed3319a1fb46b9fa02c4c111525c98be2446
David Benitez <dbr.bcn@gmail.com>
2024-07-14 07:17:42 +0200

    adds translations

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,31 +1,39 @@
 export default defineI18nConfig(() => ({
-    legacy: false,
-    locale: 'es',
-    messages: {
-      en: { // English
-        
-      },
-      es: { // Spanish
-        landing: {
-          title: "NPC CALCULATOR",
-          'sub-title': "¿Es tu amigo un NPC?"
-        },
-        footer: {
-          copy: "NPC Calculator ©",
-          about: "Sobre nosotros",
-          privacy: "Política de privacidad"
+  legacy: false,
+  locale: 'es',
+  messages: {
+    en: { // English
+
+    },
+    es: { // Spanish
+      landing: {
+        title: "NPC CALCULATOR",
+        'sub-title': "¿Es tu amigo un NPC?",
+        content: {
+          title1: "¿Que es un NPC?",
+          body1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et pulvinar orci. Maecenas luctus maximus ultrices. Fusce efficitur augue mollis orci sodales, vitae volutpat diam tempor.",
+          title2: "¿Como funciona NPC calculator?",
+          body2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et pulvinar orci. Maecenas luctus maximus ultrices. Fusce efficitur augue mollis orci sodales, vitae volutpat diam tempor.",
+          body3: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et pulvinar orci. Maecenas luctus maximus ultrices. Fusce efficitur augue mollis orci sodales, vitae volutpat diam tempor.",
+          btn3: "Calcular ahora"
         }
       },
-      ca: { // Catalan
-
-      },
-      de: { // German
-
-      },
-      fr: { //French
-      },
-      pt: { // Portuguese
-
+      footer: {
+        copy: "NPC Calculator ©",
+        about: "Sobre nosotros",
+        privacy: "Política de privacidad"
       }
+    },
+    ca: { // Catalan
+
+    },
+    de: { // German
+
+    },
+    fr: { //French
+    },
+    pt: { // Portuguese
+
     }
-  }))
+  }
+}))
