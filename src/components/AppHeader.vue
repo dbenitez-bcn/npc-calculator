4409ddfe951e2900d1e629c9cee01f918d3f37e6
David Benitez <dbr.bcn@gmail.com>
2024-07-14 05:43:23 +0200

    updates app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,26 @@
 <template>
-    <div class="app-header pa-4">
-        <LanguageSelector />
-        <p class="text-h3 text-md-center">{{ $t('landing.title') }}</p>
-        <p class="text-h6 text-md-center">{{ $t('landing.sub-title') }}</p>
-    </div>
+  <div class="app-header">
+    <v-container class="pa-4 d-flex flex-column">
+      <LanguageSelector />
+      <v-row class="justify-sm-center justify-sx-start">
+        <v-col cols="8">
+          <p class="text-h3">{{ $t("landing.title") }}</p>
+          <p class="text-h6">{{ $t("landing.sub-title") }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
 .app-header {
-    background-image: url("~/assets/img/header-bg.jpg");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
+  background-image: url("~/assets/img/header-bg.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 </style>
--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,21 +1,41 @@
 <template>
-    <div>
-        <p v-click-outside="onClickOutisde" class="bg-green">selector</p>
-    </div>
-    
+  <v-row class="justify-end ma-0">
+    <v-col
+      v-on:click="onClick"
+      v-click-outside="onClickOutisde"
+      cols="2"
+      sm="1"
+      class="bg-white rounded d-flex justify-center align-center"
+    >
+      <v-img
+        class="ml-2"
+        :width="'75%'"
+        max-height="30"
+        aspect-ratio="16/9"
+        src="~/assets/img/es-flag.svg"
+      ></v-img>
+      <v-icon icon="mdi-chevron-down"></v-icon>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-    data: {
-
+  data: () => {
+    isOpen: false;
+  },
+  methods: {
+    onClickOutisde: function (e) {
+      this.isOpen = false;
+      console.log("Is popup open: ", this.isOpen);
     },
-    methods: {
-        onClickOutisde: function(e) {
-            console.log("Hello");
-        }
-    }
-}
+    onClick: function (e) {
+      this.isOpen = true;
+      console.log("Is popup open: ", this.isOpen);
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+</style>
