d6b2e6850326a2097a6b2660b45948df95d74c65
David Benitez <dbr.bcn@gmail.com>
2024-07-31 00:59:29 +0200

    creates friend button

---
+++ src/components/FriendButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="friend-button-container rounded-lg bg-primary border-md border-secondary d-flex justify-space-between align-center cursor-pointer border-opacity-100"
+    :class="isReverse ? 'flex-row-reverse right-radius-bg' : 'flex-row left-radius-bg'">
+    <div class="img-container">
+      <v-img :src="imgSrc" :height="'100%'" cover/>
+    </div>
+    <p class="text-md-h5 text-h6 px-4 align-self-center">{{ $t(title) }}</p>
+    <i></i>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  imgSrc: String,
+  title: String,
+  isReverse: Boolean
+})
+</script>
+
+<style>
+.img-container {
+  width: 100px;
+  height: 100px;
+  transition: all 0.3s ease-out;
+}
+.friend-button-container {
+  height: 100px;
+  overflow: hidden;
+}
+.right-radius-bg:hover {
+  background: radial-gradient(at right top, rgb(var(--v-theme-soft-thunder)), rgb(var(--v-theme-night-blue)));
+}
+.left-radius-bg:hover {
+  background: radial-gradient(at left top, rgb(var(--v-theme-soft-thunder)), rgb(var(--v-theme-night-blue)));
+}
+.friend-button-container:hover .img-container {
+  transform: scale(1.25);
+  transition: transform 0.3s ease-in-out;
+}
+</style>
