0d6d8240d42e0ab7b77d89d2a923d62d1067e90e
David Benitez <dbr.bcn@gmail.com>
2024-07-31 01:00:58 +0200

    adds NPC form

---
+++ src/components/NpcForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ isMale ? "Male!" : "Female!" }}
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    isMale: Boolean
+})
+
+</script>
+
+<style lang="scss" scoped></style>
