cab10daa6ce8a97a4b44b3132ea6cc59242a4b5d
David Benitez <dbr.bcn@gmail.com>
2024-07-31 13:20:51 +0200

    creates app logo

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLink :to="localePath('/')">
+        <v-img src="~/assets/img/app-logo.png" aspect-ratio="1" :max-height="'105px'"/>
+    </NuxtLink>
+</template>
+
+<script setup>
+</script>
+
+<style></style>
