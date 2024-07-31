3eb2bd452f2b0df2dfb74ed444de5f6ef20df020
David Benitez <dbr.bcn@gmail.com>
2024-07-31 13:20:58 +0200

    adds app logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="app-header">
     <v-container class="pa-4 d-flex flex-column">
-      <LanguageSelector />
-      <v-row class="justify-sm-center justify-sx-start">
-        <v-col cols="8">
+      <v-row class="ma-0" justify-sm="end" justify="space-between">
+        <v-col class="pa-0 d-block d-sm-none" cols="2">
+          <AppLogo />
+        </v-col>
+        <v-col cols="4" sm="2" xl="1" class="d-flex justify-center align-self-center">
+          <LanguageSelector />
+        </v-col>
+      </v-row>
+      <v-row class="justify-lg-center justify-sx-start">
+        <v-col class="d-none d-sm-block pa-0" cols="1" sm="2" lg="1">
+          <AppLogo />
+        </v-col>
+        <v-col class="d-flex flex-column justify-center" lg="7">
           <h1 class="text-h3">{{ $t("landing.title") }}</h1>
           <h2 class="text-h6">{{ $t("landing.sub-title") }}</h2>
         </v-col>
--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,23 +1,19 @@
 <template>
-  <v-row class="ma-0" justify="end">
-    <v-col cols="4" sm="2" xl="1" class="d-flex justify-center">
-      <v-menu transition="scale-transition">
-        <template v-slot:activator="{ props }">
-          <div v-bind="props"
-            class="bg-white rounded d-flex justify-center align-center py-2 px-4 position-relative cursor-pointer">
-            <v-img class="ml-2" :width="'24px'" aspect-ratio="16/9" :src="`/img/flag-${locale}.png`"></v-img>
-            <v-icon icon="mdi-chevron-down"></v-icon>
-          </div>
-        </template>
-        <v-list nav :min-width="'200px'">
-          <v-list-item v-for="(code, i) in availableLocales" :key="i" color="primary" :active="code == locale"
-            @click="setLocale(code)">
-            <v-list-item-title>{{ getLanguageName(code) }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-col>
-  </v-row>
+  <v-menu transition="scale-transition">
+    <template v-slot:activator="{ props }">
+      <div v-bind="props"
+        class="bg-white rounded d-flex justify-center align-center py-2 px-4 position-relative cursor-pointer">
+        <v-img class="ml-2" :width="'24px'" aspect-ratio="16/9" :src="`/img/flag-${locale}.png`"></v-img>
+        <v-icon icon="mdi-chevron-down"></v-icon>
+      </div>
+    </template>
+    <v-list nav :min-width="'200px'">
+      <v-list-item v-for="(code, i) in availableLocales" :key="i" color="primary" :active="code == locale"
+        @click="setLocale(code)">
+        <v-list-item-title>{{ getLanguageName(code) }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
 
 <script setup>
