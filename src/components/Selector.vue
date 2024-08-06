7fd4a9b7b94662cbc0f61e77077cee43b21ba7d3
David Benitez <dbr.bcn@gmail.com>
2024-08-06 18:12:14 +0200

    improves ui

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-inline-flex border-md border-primary border-opacity-100 rounded">
+  <div class="d-inline-flex justify-space-between border-md border-primary border-opacity-100 rounded-lg" :class="fillWith ? 'w-100' : ''">
     <div v-for="(item, i) in items" :key="i" class="cursor-pointer" :class="selected == i ? 'bg-primary' : ''"
       @click="$emit('onSelectedUpdate', i)">
       <v-icon :icon="item.src" size="42" class="ma-4"></v-icon>
