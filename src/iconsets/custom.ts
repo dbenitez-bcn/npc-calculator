36c6b144767b2dd2c11ace97755dee593a7d822f
David Benitez <dbr.bcn@gmail.com>
2024-08-06 17:48:31 +0200

    adds hair icons

--- src/iconsets/custom.ts
+++ src/iconsets/custom.ts
@@ -1,7 +1,17 @@
 import male from "~/components/icons/male.vue";
 import female from "~/components/icons/female.vue";
+import hair1 from "~/components/icons/hair1.vue";
+import hair2 from "~/components/icons/hair2.vue";
+import hair3 from "~/components/icons/hair3.vue";
+import hair4 from "~/components/icons/hair4.vue";
+import hair5 from "~/components/icons/hair5.vue";
 
 export const customAliases = {
     male,
-    female
+    female,
+    hair1,
+    hair2,
+    hair3,
+    hair4,
+    hair5,
 };
