f50e2ea6d417bce8b75afc958d15ce779c21adb6
David Benitez <dbr.bcn@gmail.com>
2024-08-07 10:00:57 +0200

    Adds icons

--- src/iconsets/custom.ts
+++ src/iconsets/custom.ts
@@ -1,3 +1,18 @@
+import hobbie5 from '~/components/icons/hobbie5.vue';
+import hobbie4 from '~/components/icons/hobbie4.vue';
+import hobbie3 from '~/components/icons/hobbie3.vue';
+import hobbie2 from '~/components/icons/hobbie2.vue';
+import hobbie1 from '~/components/icons/hobbie1.vue';
+import social5 from '~/components/icons/social5.vue';
+import social4 from '~/components/icons/social4.vue';
+import social3 from '~/components/icons/social3.vue';
+import social2 from '~/components/icons/social2.vue';
+import social1 from '~/components/icons/social1.vue';
+import music5 from '~/components/icons/music5.vue';
+import music4 from '~/components/icons/music4.vue';
+import music3 from '~/components/icons/music3.vue';
+import music2 from '~/components/icons/music2.vue';
+import music1 from '~/components/icons/music1.vue';
 import male from "~/components/icons/male.vue";
 import female from "~/components/icons/female.vue";
 import hair1 from "~/components/icons/hair1.vue";
@@ -12,6 +27,21 @@ export const customAliases = {
     hair1,
     hair2,
     hair3,
+    music1,
+    music2,
+    music3,
+    music4,
+    music5,
+    social1,
+    social2,
+    social3,
+    social4,
+    social5,
+    hobbie1,
+    hobbie2,
+    hobbie3,
+    hobbie4,
+    hobbie5,
     hair4,
     hair5,
 };
