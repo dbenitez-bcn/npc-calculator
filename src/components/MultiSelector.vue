b6a35088ba9d0ad8c367c33d4c20a4f5110e8751
David Benitez <dbr.bcn@gmail.com>
2024-08-08 01:28:25 +0200

    multiselector WIP

---
+++ src/components/MultiSelector.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d-flex justify-space-between">
+        <div class="d-flex flex-column align-center" v-for="(item, i ) in items" :key="i">
+            <div class="border-md border-primary border-opacity-100 rounded-lg cursor-pointer" @click="$emit('onToggle', i)" :class=" selecteds[i] ? 'bg-primary' : ''">
+                <v-icon :icon="item.src" size="42" class="ma-4" />
+            </div>
+            <span class="text-h6" v-if="item.label">{{ $t(item.label) }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    items: Array,
+    selecteds: Array
+})
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/calculate.vue
+++ src/pages/calculate.vue
@@ -2,9 +2,18 @@
   <v-container>
     <v-row>
       <v-col>
-        <Selector :items="genderButons" :selected="friend.isMale ? 1 : 0" @onSelectedUpdate="updateGender" />
+        <Selector :items="genderButtons" :selected="friend.isMale ? 1 : 0" @onSelectedUpdate="updateGender" />
         <h3 class="w-100"> {{ $t("calculate.content.title1") }}</h3>
-        <Selector :items="hairButons" :selected="friend.hair" :fillWith="true" @onSelectedUpdate="updateHair" />
+        <MultiSelector :items="musicButtons" :selecteds="friend.music" @onToggle="toggleMusic" />
+        <Divider />
+        <h3 class="w-100"> {{ $t("calculate.content.title2") }}</h3>
+        <MultiSelector :items="socialButtons" :selecteds="friend.social" @onToggle="toggleSocial" />
+        <Divider />
+        <h3 class="w-100"> {{ $t("calculate.content.title1") }}</h3>
+        <Selector :items="hairButtons" :selected="friend.hair" :fillWith="true" @onSelectedUpdate="updateHair" />
+        <Divider />
+        <h3 class="w-100"> {{ $t("calculate.content.title4") }}</h3>
+        <MultiSelector :items="hobbieButtons" :selecteds="friend.hobbie" @onToggle="toggleHobbie" />
         <Divider />
         <v-btn class="my-4" variant="flat" rounded="xl" color="primary">{{
           $t("calculate.content.btn1")
@@ -27,13 +36,13 @@ definePageMeta({
   headerSubtitle: ''
 })
 
-const genderButons = [{
+const genderButtons = [{
   src: "$female"
 }, {
   src: "$male"
 }]
 
-const hairButons = [{
+const hairButtons = [{
   src: "$hair1"
 }, {
   src: "$hair2"
@@ -43,13 +52,71 @@ const hairButons = [{
   src: "$hair4"
 }, {
   src: "$hair5"
-},]
+}]
+
+const socialButtons = [{
+  src: "$social1"
+}, {
+  src: "$social2"
+}, {
+  src: "$social3"
+}, {
+  src: "$social4"
+}, {
+  src: "$social5"
+}]
+
+const musicButtons = [{
+  src: "$music1",
+  label: "calculate.content.music.label1"
+}, {
+  src: "$music2",
+  label: "calculate.content.music.label2"
+}, {
+  src: "$music3",
+  label: "calculate.content.music.label3"
+}, {
+  src: "$music4",
+  label: "calculate.content.music.label4"
+}, {
+  src: "$music5",
+  label: "calculate.content.music.label5"
+}]
+
+const hobbieButtons = [{
+  src: "$hobbie1",
+  label: "calculate.content.hobbie.label1"
+}, {
+  src: "$hobbie2",
+  label: "calculate.content.hobbie.label2"
+}, {
+  src: "$hobbie3",
+  label: "calculate.content.hobbie.label3"
+}, {
+  src: "$hobbie4",
+  label: "calculate.content.hobbie.label4"
+}, {
+  src: "$hobbie5",
+  label: "calculate.content.hobbie.label5"
+}]
 
 function updateGender(index) {
   friend.setGender(index == 1 ? Gender.male : Gender.female);
 }
 function updateHair(index) {
   friend.hair = index
+}
+
+function toggleMusic(index) {
+  friend.music[index] = !friend.music[index];
+}
+
+function toggleSocial(index) {
+  friend.social[index] = !friend.social[index];
+}
+
+function toggleHobbie(index) {
+  friend.hobbie[index] = !friend.hobbie[index];
 }
 </script>
 
