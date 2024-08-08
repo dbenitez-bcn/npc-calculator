87ea550c9e38b0d7ae18733cd8614a428e9014e4
David Benitez <dbr.bcn@gmail.com>
2024-08-08 02:38:38 +0200

    fix SVGs

--- src/components/icons/hobbie2.vue
+++ src/components/icons/hobbie2.vue
@@ -1,17 +1,69 @@
 <template>
-<svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_167_285)">
-<path d="M40.5563 32.983L36.6188 13.4268C36.225 11.7205 35.175 10.1455 33.7313 9.22678C31.2375 7.78303 28.0875 8.17678 26.1188 10.1455L24.4125 11.983H21.7875H20.3438H17.7188L15.8813 10.1455C13.9125 8.17678 10.7625 7.78303 8.26877 9.22678C6.69377 10.1455 5.64377 11.5893 5.38127 13.4268L1.44377 32.983C1.05002 35.083 2.62502 36.9205 4.72502 36.9205C5.51252 36.9205 6.16877 36.658 6.69377 36.0018L11.55 30.358C13.7813 27.8643 16.9313 26.4205 20.2125 26.4205H21.6563C24.9375 26.4205 28.2188 27.8643 30.3188 30.358L35.3063 36.0018C35.8313 36.5268 36.4875 36.9205 37.275 36.9205C39.5063 36.9205 41.0813 34.9518 40.5563 32.983Z" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M30.1875 21.1705C31.6372 21.1705 32.8125 19.9953 32.8125 18.5455C32.8125 17.0958 31.6372 15.9205 30.1875 15.9205C28.7378 15.9205 27.5625 17.0958 27.5625 18.5455C27.5625 19.9953 28.7378 21.1705 30.1875 21.1705Z" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M11.8125 21.1705V15.9205" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M14.4375 18.5455H9.1875" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M22.3125 17.233H19.6875" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M22.3125 21.1705H19.6875" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-</g>
-<defs>
-<clipPath id="clip0_167_285">
-<rect width="42" height="42" fill="white" transform="translate(0 0.170532)"/>
-</clipPath>
-</defs>
-</svg>
+  <svg
+    viewBox="0 0 42 42"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style="width: 100%; height: 100%"
+  >
+    <g clip-path="url(#clip0_167_285)">
+      <path
+        d="M40.5563 32.983L36.6188 13.4268C36.225 11.7205 35.175 10.1455 33.7313 9.22678C31.2375 7.78303 28.0875 8.17678 26.1188 10.1455L24.4125 11.983H21.7875H20.3438H17.7188L15.8813 10.1455C13.9125 8.17678 10.7625 7.78303 8.26877 9.22678C6.69377 10.1455 5.64377 11.5893 5.38127 13.4268L1.44377 32.983C1.05002 35.083 2.62502 36.9205 4.72502 36.9205C5.51252 36.9205 6.16877 36.658 6.69377 36.0018L11.55 30.358C13.7813 27.8643 16.9313 26.4205 20.2125 26.4205H21.6563C24.9375 26.4205 28.2188 27.8643 30.3188 30.358L35.3063 36.0018C35.8313 36.5268 36.4875 36.9205 37.275 36.9205C39.5063 36.9205 41.0813 34.9518 40.5563 32.983Z"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M30.1875 21.1705C31.6372 21.1705 32.8125 19.9953 32.8125 18.5455C32.8125 17.0958 31.6372 15.9205 30.1875 15.9205C28.7378 15.9205 27.5625 17.0958 27.5625 18.5455C27.5625 19.9953 28.7378 21.1705 30.1875 21.1705Z"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.8125 21.1705V15.9205"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.4375 18.5455H9.1875"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22.3125 17.233H19.6875"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22.3125 21.1705H19.6875"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_167_285">
+        <rect
+          width="42"
+          height="42"
+          fill="white"
+          transform="translate(0 0.170532)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
 </template>
--- src/components/icons/music1.vue
+++ src/components/icons/music1.vue
@@ -1,8 +1,41 @@
 <template>
-<svg width="800" height="800" viewBox="0 0 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M400 725C579.493 725 725 579.493 725 400C725 220.507 579.493 75 400 75C220.507 75 75 220.507 75 400C75 579.493 220.507 725 400 725Z" stroke="white" stroke-width="50" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M400 500C455.228 500 500 455.228 500 400C500 344.772 455.228 300 400 300C344.772 300 300 344.772 300 400C300 455.228 344.772 500 400 500Z" stroke="white" stroke-width="50" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M400 150C262.5 150 150 262.5 150 400" stroke="white" stroke-width="50" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M400 225C302.5 225 225 302.5 225 400" stroke="white" stroke-width="50" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+  <svg
+    viewBox="0 0 800 800"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style="width: 100%; height: 100%"
+  >
+    <path
+      d="M400 725C579.493 725 725 579.493 725 400C725 220.507 579.493 75 400 75C220.507 75 75 220.507 75 400C75 579.493 220.507 725 400 725Z"
+      stroke="white"
+      stroke-width="50"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M400 500C455.228 500 500 455.228 500 400C500 344.772 455.228 300 400 300C344.772 300 300 344.772 300 400C300 455.228 344.772 500 400 500Z"
+      stroke="white"
+      stroke-width="50"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M400 150C262.5 150 150 262.5 150 400"
+      stroke="white"
+      stroke-width="50"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M400 225C302.5 225 225 302.5 225 400"
+      stroke="white"
+      stroke-width="50"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
 </template>
