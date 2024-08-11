ec24bf821ecce279654ba00c9ee2c06c3e66e4f3
David Benitez <dbr.bcn@gmail.com>
2024-08-11 22:01:58 +0200

    adds result page

---
+++ src/pages/result.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col class="d-flex flex-column align-center" cols="6">
+        <NpcScoreIndicator />
+        <div class="d-flex flex-column align-center mt-2">
+          <v-btn class="my-2 align-self-stretch" variant="flat" rounded="xl" color="primary" @click="toCalculate">{{
+            $t("result.content.btn1") }}</v-btn>
+          <v-btn class="my-2 align-self-stretch" variant="outlined" rounded="xl" color="primary">{{
+            $t("result.content.btn2") }}</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+const friend = useFriend();
+const { t, locale } = useI18n();
+
+useHead({
+  title: t(`result.title_${friend.gender}`)
+})
+
+definePageMeta({
+  title: `result.title_`,
+  headerSubtitle: ''
+})
+
+function toCalculate() {
+  navigateTo(`/${locale.value}/calculate`);
+}
+
+</script>
+
+<style lang="scss" scoped></style>
