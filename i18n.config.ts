918217d848c547f2388bede2008c17aaf6ecc629
David Benitez <dbr.bcn@gmail.com>
2024-08-12 23:30:45 +0200

    adds translations for all available languages

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,4 +1,10 @@
+import en from "~/locales/en.json";
 import es from "~/locales/es.json";
+import ca from "~/locales/ca.json";
+import de from "~/locales/de.json";
+import fr from "~/locales/fr.json";
+import pt from "~/locales/pt.json";
+import it from "~/locales/it.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
@@ -6,23 +12,12 @@ export default defineI18nConfig(() => ({
   fallbackLocale: 'en',
   warnHtmlMessage: false, 
   messages: {
-    en: { // English
-
-    },
+    en,
     es,
-    ca: { // Catalan
-
-    },
-    de: { // German
-
-    },
-    fr: { //French
-    },
-    pt: { // Portuguese
-
-    },
-    it: { // italian
-
-    }
+    ca,
+    de,
+    fr,
+    pt,
+    it,
   }
 }))
