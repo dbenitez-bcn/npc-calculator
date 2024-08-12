621e94ebeaabe74abceca4ce10d0c1296b97332b
David Benitez <dbr.bcn@gmail.com>
2024-08-12 13:22:32 +0200

    upgrate npc formula

--- src/stores/friend.test.ts
+++ src/stores/friend.test.ts
@@ -1,5 +1,5 @@
 import { setActivePinia, createPinia } from 'pinia'
-import { Gender, useFriend } from './friend'
+import { Gender, useFriend, Hair } from './friend'
 import { describe, beforeEach, it, expect } from 'vitest';
 describe('Friend store', () => {
     beforeEach(() => {
@@ -46,7 +46,8 @@ describe('Friend store', () => {
             sut.music = [false, true, false, false, false, false];
             sut.social = [false, false, false, true, false, false];
             sut.hobbie = [false, false, false, false, true, false];
-            sut.hair = 4;
+            sut.setGender(Gender.female);
+            sut.hair = 0;
 
             expect(sut.npcScore).toBe(100);
         });
@@ -59,6 +60,44 @@ describe('Friend store', () => {
 
             expect(sut.npcScore).toBe(0);
         });
+
+        it.each([
+            [50, Hair.black, Gender.male],
+            [45, Hair.brown, Gender.male],
+            [45, Hair.blond, Gender.male],
+            [40, Hair.red, Gender.male],
+            [55, Hair.pink, Gender.male],
+            [50, Hair.black, Gender.female],
+            [40, Hair.brown, Gender.female],
+            [55, Hair.blond, Gender.female],
+            [40, Hair.red, Gender.female],
+            [55, Hair.pink, Gender.female],
+        ])('Should return %i when hair is %i and gender %s with default values', (score, hair, gender) => {
+            const sut = useFriend();
+            sut.hair = hair;
+            sut.setGender(gender)
+
+            expect(sut.npcScore).toBe(score);
+        });
+    })
+
+    it('Should maintain the score under 100', () => {
+        const sut = useFriend();
+        sut.music = [true, true, false, true, false, false];
+        sut.social = [false, false, true, true, true, false];
+        sut.hobbie = [false, true, false, true, true, false];
+        sut.hair = 4;
+
+        expect(sut.npcScore).toBe(95);
+    })
+    it('Should maintain the score above 0', () => {
+        const sut = useFriend();
+        sut.music = [false, false, true, false, true, true];
+        sut.social = [true, false, false, false, false, true];
+        sut.hobbie = [true, false, true, false, false, true];
+        sut.hair = 4;
+
+        expect(sut.npcScore).toBe(5);
     })
 
     describe('$reset', () => {
--- src/stores/friend.ts
+++ src/stores/friend.ts
@@ -4,10 +4,17 @@ export enum Gender {
     male = 'm',
     female = 'f'
 }
+export enum Hair {
+    black = 0,
+    brown = 1,
+    blond = 2,
+    red = 3,
+    pink = 4
+}
 
 export const useFriend = defineStore('friend', () => {
     const gender = ref(Gender.male);
-    const hair = ref(0);
+    const hair = ref(Hair.black);
     const music = ref<boolean[]>([]);
     const social = ref<boolean[]>([]);
     const hobbie = ref<boolean[]>([]);
@@ -21,13 +28,63 @@ export const useFriend = defineStore('friend', () => {
         const musicScore = getScore(music.value, [22, 50, -36, 38, -50, -24]) * 0.34;
         const socialScore = getScore(social.value, [-50, 0, 12, 50, 29, -41]) * 0.33;
         const hobbieScore = getScore(hobbie.value, [-3, 5, -19, 17, 50, -50]) * 0.33;
-        return Math.floor(musicScore + hobbieScore + socialScore);
+        return scoreRectifier((musicScore + hobbieScore + socialScore) * getHairMultiplier());
     });
 
     function getScore(activeStats: boolean[], values: number[]): number {
         return activeStats.reduce((sum, isTrue, index) => {
             return isTrue ? sum + values[index] : sum;
         }, 50);
+    }
+
+    function getHairMultiplier(): number {
+        let multiplier = 1;
+
+        if (isMale.value) {
+            switch (hair.value) {
+                case Hair.black:
+                    multiplier = 1;
+                    break;
+                case Hair.brown:
+                case Hair.blond:
+                    multiplier = 0.9;
+                    break;
+                case Hair.red:
+                    multiplier = 0.8;
+                    break;
+                case Hair.pink:
+                    multiplier = 1.1;
+                    break;
+            }
+        } else {
+            switch (hair.value) {
+                case Hair.black:
+                    multiplier = 1;
+                    break;
+                case Hair.brown:
+                case Hair.red:
+                    multiplier = 0.8;
+                    break;
+                case Hair.blond:
+                case Hair.pink:
+                    multiplier = 1.1;
+                    break;
+            }
+        }
+        return multiplier;
+    }
+
+    function scoreRectifier(score: number): number {
+        let rectified = score;
+
+        if (score > 100) {
+            rectified = 90 + (score - 100) / 10;
+        } else if (score < 0) {
+            rectified = (100 + score) / 10;
+        }
+        const isBetween0And100 = rectified >= 0 && rectified <= 100;
+
+        return isBetween0And100 ? Math.floor(rectified) : scoreRectifier(rectified);
     }
 
     function $reset() {
