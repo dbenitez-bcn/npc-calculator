a8c4cdecbc635ae4ecfad284810ff5872202c867
David Benitez <dbr.bcn@gmail.com>
2024-08-13 00:16:43 +0200

    adds feature toggle

--- src/pages/result.vue
+++ src/pages/result.vue
@@ -6,7 +6,7 @@
         <div class="d-flex flex-column align-center mt-2">
           <v-btn class="my-2 align-self-stretch" variant="flat" rounded="xl" color="primary" @click="toCalculate">{{
             $t("result.content.btn1") }}</v-btn>
-          <v-btn class="my-2 align-self-stretch" variant="outlined" rounded="xl" color="primary">{{
+          <v-btn v-if="isDev" class="my-2 align-self-stretch" variant="outlined" rounded="xl" color="primary">{{
             $t("result.content.btn2") }}</v-btn>
         </div>
       </v-col>
@@ -17,6 +17,7 @@
 <script setup>
 const friend = useFriend();
 const { t, locale } = useI18n();
+const isDev = import.meta.dev;
 
 useHead({
   title: t(`result.title_${friend.gender}`)
