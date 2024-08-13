894c8a1fdb5c5c0a605bb7df1a022287dd9da931
David Benitez <dbr.bcn@gmail.com>
2024-08-13 14:03:41 +0200

    disables analitics in dev

--- src/plugins/analytics.client.ts
+++ src/plugins/analytics.client.ts
@@ -1,25 +1,20 @@
 import {
-    type Analytics,
-    initializeAnalytics,
-    isSupported,
-  } from 'firebase/analytics'
-  
-  export default defineNuxtPlugin(async () => {
+  type Analytics,
+  initializeAnalytics,
+  isSupported,
+} from 'firebase/analytics'
+
+export default defineNuxtPlugin(async () => {
+  let analytics: Analytics | null = null
+  if (!import.meta.dev) {
     const firebaseApp = useFirebaseApp()
-  
-    console.log('Loading analytics')
-  
-    let analytics: Analytics | null = null
     if (await isSupported()) {
       analytics = initializeAnalytics(firebaseApp)
-      console.log('Loaded analytics')
-    } else {
-      console.log('Analytics not supported')
     }
-  
-    return {
-      provide: {
-        analytics,
-      },
-    }
-  })
+  }
+  return {
+    provide: {
+      analytics,
+    },
+  }
+})
