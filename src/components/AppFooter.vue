738f009c71b668938db85802254a317b4b21e8b6
David Benitez <dbr.bcn@gmail.com>
2024-08-14 02:30:51 +0200

    adds privacy policy link

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,14 @@
 <template>
   <v-footer class="bg-primary">
     <div class="text-center w-100">
-        <!-- TODO: Add links -->
-      <strong>{{$t("footer.copy")}}</strong> · {{$t("footer.privacy") }} · {{$t("footer.about") }}
+      <!-- TODO: Add links -->
+      <strong>{{ $t("footer.copy") }}</strong> ·
+      <a
+        class="text-white"
+        href="https://www.termsfeed.com/live/6769f407-753f-44b3-b833-7d8190fb40c5"
+        >{{ $t("footer.privacy") }}</a
+      >
+      <!-- · {{ $t("footer.about") }} -->
     </div>
   </v-footer>
 </template>
