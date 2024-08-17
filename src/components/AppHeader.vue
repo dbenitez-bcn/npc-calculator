bc089a7a683be0d45b534a90d72130488cf0d12c
David Benitez <dbr.bcn@gmail.com>
2024-08-17 13:39:38 +0200

    adds error page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,7 +19,7 @@
           <AppLogo />
         </v-col>
         <v-col class="d-flex flex-column justify-center" lg="9">
-          <h1 class="text-h3">{{ $t(route.meta.title+friend.gender) }}</h1>
+          <h1 class="text-h3">{{ $t(route.meta.title ? route.meta.title+friend.gender : "NPC Calculator") }}</h1>
           <h2 v-if="route.meta.headerSubtitle" class="text-h6">{{ $t(route.meta.headerSubtitle) }}</h2>
         </v-col>
       </v-row>
---
+++ src/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <NuxtLayout>
+      <AppHeader />
+      <v-container>
+        <v-row>
+          <v-col class="d-flex flex-column align-center vh-75 justify-center">
+            <span class="text-h3 text-center">{{ $t("error.content.title") }}</span>
+            <span class="my-2 text-h4 font-weight-bold">{{ error.statusCode }}</span>
+            <v-btn class="my-2" variant="outlined" rounded="xl" color="primary" @click="navigateTo(localePath('/'))">{{
+              $t("error.content.btn") }}</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+      <NuxtRouteAnnouncer />
+      <AppFooter />
+    </NuxtLayout>
+  </v-app>
+</template>
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+</script>
+<style scoped>
+.vh-75 {
+  height: 65dvh
+}
+</style>
