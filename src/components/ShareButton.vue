71c423f63154502fff20b75faf56dfab89431a45
David Benitez <dbr.bcn@gmail.com>
2024-08-17 06:05:42 +0200

    ads share button

---
+++ src/components/ShareButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog max-width="500">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        v-bind="activatorProps"
+        class="my-2 align-self-stretch"
+        variant="outlined"
+        rounded="xl"
+        color="primary"
+        >{{ $t("result.content.btn2") }}</v-btn
+      >
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card title="Dialog">
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/result.vue
+++ src/pages/result.vue
@@ -4,10 +4,15 @@
       <v-col class="d-flex flex-column align-center" cols="6">
         <NpcScoreIndicator />
         <div class="d-flex flex-column align-center mt-2">
-          <v-btn class="my-2 align-self-stretch" variant="flat" rounded="xl" color="primary" @click="toCalculate">{{
-            $t("result.content.btn1") }}</v-btn>
-          <v-btn v-if="isDev" class="my-2 align-self-stretch" variant="outlined" rounded="xl" color="primary">{{
-            $t("result.content.btn2") }}</v-btn>
+          <v-btn
+            class="my-2 align-self-stretch"
+            variant="flat"
+            rounded="xl"
+            color="primary"
+            @click="toCalculate"
+            >{{ $t("result.content.btn1") }}</v-btn
+          >
+          <ShareButton v-if="isDev" />
         </div>
       </v-col>
     </v-row>
@@ -20,20 +25,19 @@ const { t, locale } = useI18n();
 const isDev = import.meta.dev;
 
 useHead({
-  title: t(`result.title_${friend.gender}`)
-})
+  title: t(`result.title_${friend.gender}`),
+});
 
 definePageMeta({
   title: `result.title_`,
-  headerSubtitle: ''
-})
+  headerSubtitle: "",
+});
 
 function toCalculate() {
   const localePath = useLocalePath();
-  navigateTo(localePath('/calculate'));
+  navigateTo(localePath("/calculate"));
   friend.$reset();
 }
-
 </script>
 
 <style lang="scss" scoped></style>
