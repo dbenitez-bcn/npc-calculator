a1dc24a45735bf05b0831ef17af0b33d48f0c5e9
David Benitez <dbr.bcn@gmail.com>
2024-08-27 11:11:09 +0200

    justify items correctly

--- src/components/MultiSelector.vue
+++ src/components/MultiSelector.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="pa-0">
-        <v-row>
+        <v-row justify="center">
             <v-col class="d-flex flex-column align-center pt-0" v-for="(item, i ) in items" :key="i" cols="4">
                 <div class="border-md border-primary rounded-lg cursor-pointer" @click="$emit('onToggle', i)" :class=" selecteds[i] ? 'bg-primary' : ''">
                 <v-icon :icon="item.src" size="42" class="ma-4" />
